8f525153c0aca0fbe7ac855505c82bb2afebbd50
Eduardo <princesherlockk@gmail.com>
2024-07-19 14:43:28 -0300

    [NLW-Journey-frontend]-feat- my first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+
+  return (
+    <>
+      <h1 className="font-bold underline">
+        Parei no minuto <span className="text-red-800">19:50</span> do video
+      </h1>
+      <h2>Ainda não commintei nada deste projeto, nem criei repositório</h2>
+    </>
+  )
+}
