042bc8e6d784b61f971bac5dbdb9f11189802eda
Eduardo <princesherlockk@gmail.com>
2024-07-19 16:02:54 -0300

    [NLW-Journey-frontend]-feat- created the home page layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,41 @@
+import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+
 export function App() {
 
   return (
-    <>
-      <h1 className="font-bold underline">
-        Parei no minuto <span className="text-red-800">19:50</span> do video
-      </h1>
-      <h2>Ainda não commintei nada deste projeto, nem criei repositório</h2>
-    </>
+    <div className="w-full h-screen flex items-center justify-center bg-pattern">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className='flex flex-col items-center gap-3'>
+          <img src="/logo.png" alt="logo plann.er" />
+          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        </div>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className='flex items-center gap-2 flex-1'>
+            <MapPin className='size-5 text-zinc-400' />
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none"
+            />
+          </div>
+          <div className='flex items-center gap-2'>
+            <Calendar className='size-5 text-zinc-400' />
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none w-36"
+            />
+          </div>
+          <div className='w-px h-6 bg-zinc-800 p-0.5' />
+          <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+            Continuar
+            <ArrowRight className='size-5' />
+          </button>
+        </div>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br /> com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a>
+        </p>
+      </div>
+    </div>
   )
 }
