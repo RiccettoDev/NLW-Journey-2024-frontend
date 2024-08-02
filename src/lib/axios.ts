8920878c9cd48c1c86d7825a4f3d58c30a32c425
Eduardo <princesherlockk@gmail.com>
2024-08-02 17:16:32 -0300

    [NLW-Journey-frontend]-fix- adjusted axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
+  baseURL: 'http://91.108.126.64:3001' // substitua com o IP ou domínio da sua VPS e a porta correta
+});
