4e2efc6939b42d3f3973705c4fd6d082f7e65ed9
Eduardo <princesherlockk@gmail.com>
2024-08-04 10:48:57 -0300

    [NLW-Journey-frontend]-fix- adjusted axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://riccettodev.online/' // substitua com o IP ou domínio da sua VPS e a porta correta
+  baseURL: 'https://riccettodev.online/'
 });
